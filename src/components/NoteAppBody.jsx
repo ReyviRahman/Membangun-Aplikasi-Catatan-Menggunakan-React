438cfebd6c2c592951f17b28c25b5cfef42d4b6b
Reyvi Rahman <reyvirahman@gmail.com>
2024-10-31 07:44:10 +0700

    Adding maxLength Feature

--- src/components/NoteAppBody.jsx
+++ src/components/NoteAppBody.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import NoteItem from './NoteItem'
 
 class NoteAppBody extends React.Component {
+  
   constructor(props) {
     super(props);
 
     this.state = {
       title: '',
-      body: '', 
+      body: '',
     }
 
     this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
@@ -16,11 +17,17 @@ class NoteAppBody extends React.Component {
   }
 
   onTitleChangeEventHandler(event) {
-    this.setState(() => {
-      return {
-        title: event.target.value
-      }
-    })
+    const charLimit = 50;
+
+    if (event.target.value.length <= charLimit) {
+      this.setState(() => {
+        return {
+          title: event.target.value
+        }
+      })
+    }
+
+    
   }
 
   onBodyChangeEventHandler(event) {
@@ -41,7 +48,7 @@ class NoteAppBody extends React.Component {
       <div className='note-app__body' onSubmit={this.onSubmitEventHandler}>
         <form className='note-input'>
           <h2>Buat Catatan</h2>
-          <p className='note-input__title__char-limit'>Sisa Karakter:</p>
+          <p className='note-input__title__char-limit'>Sisa Karakter: {50 - this.state.title.length}</p>
           <input className='note-input' placeholder='Ini adalah judul...' value={this.state.title} onChange={this.onTitleChangeEventHandler} />
           <textarea placeholder='Tuliskan catatanmu disini...' rows={10} value={this.state.body} onChange={this.onBodyChangeEventHandler} />
           <button type='submit'>Buat</button>
